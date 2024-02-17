82da2baac36f775665a27fe958253e9c101594f7
Charisios Charisiadis <119067361+charisios10@users.noreply.github.com>
2024-02-17 12:49:58 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "help",
+  description: "Get help for the bot commands.",
+  options: [],
+  run: async (client, interaction) => {
+    interaction.reply("This command is not available yet!");
+  },
+};
